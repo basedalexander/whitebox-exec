7c361be677750a986844332315e337766c3bde9d
Alexander Kozlov <itrefak@gmail.com>
2023-03-31 23:14:56 +0100

    basic architecture prototype

--- src/executor/executor.js
+++ src/executor/executor.js
@@ -1,12 +1,35 @@
 class Executor {
     async execute(model, params, data) {
+        const lang = model.md.lang;
+        const code = model.md.code;
+
+        const runner = this.getCodeRunner(lang);
+        const runResult = await runner.run(code, params, data);
+
         const result = {
-            proof: null,
-            paras: params,
-            data: data
+            proof: runResult.proof,
+            result: runResult.result
         };
         return result;
     }
+
+    getCodeRunner(lang) {
+        return codeRunnerRegistry[lang];
+    }
+}
+
+class RustCodeRunner {
+    // initiate risk0 instance.
+    async run(code, params, data) {
+        return {
+            result: null,
+            proof: null
+        }
+    };
+}
+
+const codeRunnerRegistry = {
+    "rust": new RustCodeRunner()
 }
 
 module.exports = Executor;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 const Executor = require('./executor/executor.js');
 const model = require('./executor/mocks/algo.mock.json');
+const publicationsResult = require('./executor/mocks/publications.mock.json');
 
-const executor = new Executor();
+const params = [
+    {
+        "name": "topics",
+        "value": ["nft", "cool"]
+    },
+    {
+        "name": "likesThreshold",
+        "value": 500
+    }
+];
+const data = publicationsResult.data.publications.items;
 
-console.log(model);
+async function main () {
+    const executor = new Executor();
+    const result = await executor.execute(model, params, data);
+    console.log(JSON.stringify(result, null, 4));
+}
+
+main();
